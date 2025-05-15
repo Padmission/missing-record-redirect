d4db03e301ebfd12c44ae0749bdf26c71707a289
Andrew Wallo <andrewdwallo@gmail.com>
2025-05-14 20:37:06 -0400

    wip

--- src/MissingRecordRedirectPlugin.php
+++ src/MissingRecordRedirectPlugin.php
@@ -2,11 +2,37 @@
 
 namespace Padmission\MissingRecordRedirect;
 
+use Closure;
 use Filament\Contracts\Plugin;
+use Filament\Notifications\Notification;
 use Filament\Panel;
+use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Concerns\EvaluatesClosures;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class MissingRecordRedirectPlugin implements Plugin
 {
+    use EvaluatesClosures;
+
+    protected ?Closure $notificationCallback = null;
+
+    protected string | Closure | null $notificationTitle = null;
+
+    protected string | Closure | null $notificationBody = null;
+
+    protected string | Closure | null $notificationType = 'warning';
+
+    protected bool | Closure $isPersistent = true;
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
     public function getId(): string
     {
         return 'missing-record-redirect';
@@ -22,9 +48,127 @@ class MissingRecordRedirectPlugin implements Plugin
         //
     }
 
-    public static function make(): static
+    public function notification(?Closure $callback = null): static
     {
-        return app(static::class);
+        $this->notificationCallback = $callback;
+
+        return $this;
+    }
+
+    public function notificationTitle(string | Closure | null $title): static
+    {
+        $this->notificationTitle = $title;
+
+        return $this;
+    }
+
+    public function getNotificationTitle(): string
+    {
+        return $this->evaluate($this->notificationTitle) ?? 'Record Deleted';
+    }
+
+    public function notificationBody(string | Closure | null $body): static
+    {
+        $this->notificationBody = $body;
+
+        return $this;
+    }
+
+    public function getNotificationBody(): string
+    {
+        return $this->evaluate($this->notificationBody) ?? 'The record you were trying to view has been deleted or does not exist.';
+    }
+
+    public function notificationType(string | Closure | null $type): static
+    {
+        $this->notificationType = $type;
+
+        return $this;
+    }
+
+    public function getNotificationType(): string
+    {
+        return $this->evaluate($this->notificationType) ?? 'warning';
+    }
+
+    public function persistent(bool | Closure $isPersistent = true): static
+    {
+        $this->isPersistent = $isPersistent;
+
+        return $this;
+    }
+
+    public function isPersistent(): bool
+    {
+        return $this->evaluate($this->isPersistent);
+    }
+
+    public function getNotification(array $data = []): Notification
+    {
+        $notification = Notification::make()
+            ->title($this->getNotificationTitle())
+            ->body($this->getNotificationBody());
+
+        $type = $this->getNotificationType();
+        if (method_exists($notification, $type)) {
+            $notification->{$type}();
+        }
+
+        if ($this->isPersistent()) {
+            $notification->persistent();
+        }
+
+        if ($this->notificationCallback !== null) {
+            $notification = $this->evaluate($this->notificationCallback, [
+                'notification' => $notification,
+                ...$data,
+            ]) ?? $notification;
+        }
+
+        return $notification;
+    }
+
+    public function handleNotFoundHttpException(NotFoundHttpException $e, Request $request, Panel $panel): ?RedirectResponse
+    {
+        $previous = $e->getPrevious();
+
+        if (! $previous instanceof ModelNotFoundException) {
+            return null;
+        }
+
+        $route = $request->route();
+
+        if (! $route instanceof Route) {
+            return null;
+        }
+
+        $controller = $route->getController();
+
+        if (! $controller instanceof ViewRecord) {
+            return null;
+        }
+
+        $resource = $controller::getResource();
+
+        $redirectUrl = $resource::getUrl();
+        $currentUrl = $request->url();
+
+        if ($redirectUrl !== $currentUrl) {
+            $model = $previous->getModel();
+
+            $notification = $this->getNotification([
+                'resource' => $resource,
+                'model' => $model,
+                'request' => $request,
+                'previousException' => $previous,
+            ]);
+
+            $notification->send();
+
+            return new RedirectResponse($redirectUrl);
+        }
+
+        return null;
     }
 
     public static function get(): static
--- src/MissingRecordRedirectServiceProvider.php
+++ src/MissingRecordRedirectServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace Padmission\MissingRecordRedirect;
 
+use Filament\Facades\Filament;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Http\Request;
 use Padmission\MissingRecordRedirect\Commands\MissingRecordRedirectCommand;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class MissingRecordRedirectServiceProvider extends PackageServiceProvider
 {
@@ -37,11 +41,36 @@ class MissingRecordRedirectServiceProvider extends PackageServiceProvider
         }
     }
 
-    public function packageRegistered(): void {}
+    public function packageRegistered(): void
+    {
+        //
+    }
 
     public function packageBooted(): void
     {
-        //
+        // We need to hook our handler into Laravel's exception handling system
+        // without replacing the existing handler or any other handlers defined by users
+        $this->app->booted(function () {
+            $handler = $this->app->make(ExceptionHandler::class);
+
+            if (method_exists($handler, 'renderable')) {
+                $handler->renderable(function (NotFoundHttpException $e, Request $request) {
+                    foreach (Filament::getPanels() as $panel) {
+                        $plugin = $panel->getPlugin('missing-record-redirect');
+
+                        if ($plugin instanceof MissingRecordRedirectPlugin) {
+                            $response = $plugin->handleNotFoundHttpException($e, $request, $panel);
+
+                            if ($response !== null) {
+                                return $response;
+                            }
+                        }
+                    }
+
+                    return null;
+                });
+            }
+        });
     }
 
     /**
